6b7cbc72dd3c1b6d839a015512dcc57d540a181c
pranaybhujade <pranaybhujade@gmail.com>
2022-09-26 19:12:47 +0530

    given 3 floats find maximum

--- generics/src/com/bridgelabz/genrics/FindMaximumMain.java
+++ generics/src/com/bridgelabz/genrics/FindMaximumMain.java
@@ -1,21 +1,25 @@
 package com.bridgelabz.genrics;
 
 public class FindMaximumMain {
-    static int findMaximumInt(Integer n1, Integer n2, Integer n3) {
-        Integer max = n1;
-        if (n2.compareTo(max) > 0) max = n2;
-        if (n3.compareTo(max) > 0) max = n3;
+    static float findMaximumFloat(Float n1, Float n2, Float n3){
+        Float max = n1;
+        if (n2.compareTo(max) > 0) max=n2;
+        if (n3.compareTo(max) > 0) max=n3;
         return max;
     }
-
     public static void main(String[] args) {
-        int num1, num2, num3;
-        num1 = 5;
-        num2 = 9;
-        num3 = 7;
+        int num1,num2,num3;
+        num1 = 5;num2 = 9;num3 = 7;
+        int maxInt = findMaximumInt(num1,num2,num3);
+        System.out.println("maximum Integer = "+maxInt);
 
-        int maximum = findMaximumInt(num1, num2, num3);
-        System.out.println("maximum = " + maximum);
+        int maximum=findMaximumInt(num1,num2,num3);
+        System.out.println("maximum = "+maximum);
+
+        float f1,f2,f3;
+        f1 = 5.1f;f2 = 6.2f;f3 = 10.5f;
+        float maxFloat = findMaximumFloat(f1,f2,f3);
+        System.out.println("maximum Float = "+maxFloat);
 
     }
 }
